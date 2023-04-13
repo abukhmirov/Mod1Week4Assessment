f7046f3eab2b6a63d2886f633287e348e24d485b
abukhmirov <alexbukhmirov@gmail.com>
2023-04-13 10:32:17 -0600

    Alex done

--- Vehicle.UnitTests/VehicleTests.cs
+++ Vehicle.UnitTests/VehicleTests.cs
@@ -12,17 +12,31 @@ namespace Vehicle.UnitTests
             Assert.Equal(0, megansCar.MilesDriven);
         }
 
-        // This test needs some work!
+
         [Fact]
         public void Summary_ReturnsInfoAboutVehicle()
         {
             Vehicle andysBike = new Vehicle(2, "Blue");
 
-            var expected = "This Blue vehicle has 2 wheels and has driven 0 miles";
+
+            Assert.Equal("This Blue vehicle has 2 wheels, and has driven 0 miles.", andysBike.Sumary());
 
 
         }
 
-        // Add more tests here!
+        [Fact]
+        public void MilesDriven_IsAddedCorrectly()
+        {
+            Vehicle megansCar = new Vehicle(2, "Blue");
+            megansCar.Drive();
+            Assert.Equal(5, megansCar.MilesDriven);
+        }
+        [Fact]
+        public void Paint_IsChangedCorrectly()
+        {
+            Vehicle megansCar = new Vehicle(4, "Green");
+            megansCar.Paint("Blue");
+            Assert.Equal("Blue", megansCar.Color);
+        }
     }
 }
